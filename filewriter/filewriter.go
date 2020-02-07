450a7ec9e54c06f76b09d74193a88bf685a95491
Marius Aarsnes <marius.aarsnes@gmail.com>
2020-02-07 23:39:33 +0100

    implement filewriter to save objective values to csv

---
+++ filewriter/filewriter.go
@@ -0,0 +1,38 @@
+package filewriter
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/CRAB-LAB-NTNU/PPS-BS/types"
+)
+
+//WriteArrayToFile writes the objective values of an array of individuals to a destination file
+func WriteArrayToFile(individuals []types.Individual, destination string) {
+
+	file, err := os.Create(destination)
+
+	checkError("cannot create file", err)
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, individual := range individuals {
+		objectiveValues := individual.Fitness().ObjectiveValues
+
+		err := writer.Write([]string{
+			strconv.FormatFloat(objectiveValues[0], 'E', -1, 64),
+			strconv.FormatFloat(objectiveValues[1], 'E', -1, 64)})
+		checkError("Cannot write to file", err)
+
+	}
+}
+
+func checkError(message string, err error) {
+	if err != nil {
+		log.Fatal(message, err)
+	}
+}
